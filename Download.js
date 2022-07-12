f09fb51c898b3188a95b2d9591b7b1130ef65022
Ameuma773 <98154029+Ameuma773@users.noreply.github.com>
2022-07-12 23:36:55 +0900

    Add files via upload

--- Download.js
+++ Download.js
@@ -2,10 +2,10 @@ function DoDownload() {
     let check = window.confirm('ダウンロード前に必ず上記の注意事項・禁止事項をお読みください。\nお読みになりましたか？');
     
     if(check) {
-        const url = 'https://github.com/Ameuma773/EarthQuickly/releases/download/v0.2.1/EarthQuickly.0.2.1.zip';
+        const url = 'https://github.com/Ameuma773/EarthQuickly/releases/download/v0.2.2/EarthQuickly.zip';
         const a = document.createElement("a");
         document.body.appendChild(a);
-        a.download = 'EarthQuickly 0.2.0.zip';
+        a.download = 'EarthQuickly 0.2.2.zip';
         a.href = url;
         a.click();
         a.remove();
