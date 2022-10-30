44d58b0fd4ea3cb5d0f35dbada54c18f3f7d1367
Ameuma773 <98154029+Ameuma773@users.noreply.github.com>
2022-10-30 12:23:01 +0900

    Add files via upload

---
+++ EarthQuickly/index.js
@@ -0,0 +1,15 @@
+function Home(){
+    location.href = "index.html";
+}
+
+function Features(){
+    location.href = "index.html#Features";
+}
+
+function Download(){
+    location.href = "download.html";
+}
+
+function Others(){
+    location.href = "others.html";
+}
