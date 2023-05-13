04451158dada38a79f5ac7782dad389590606dc1
Ameuma773 <98154029+Ameuma773@users.noreply.github.com>
2023-05-13 18:01:13 +0900

    Update Download.js

--- EarthQuickly/Download.js
+++ EarthQuickly/Download.js
@@ -2,10 +2,10 @@ function DoDownload() {
     let check = window.confirm('ダウンロード前に必ず上記の注意事項・禁止事項をお読みください。\nお読みになりましたか？');
     
     if(check) {
-        const url = 'https://github.com/Ameuma773/ameuma773.github.io/releases/download/v0.7.2/EarthQuickly.0.7.2.zip';
+        const url = 'https://github.com/Ameuma773/ameuma773.github.io/releases/download/v0.7.3/EarthQuickly.0.7.3.zip';
         const a = document.createElement("a");
         document.body.appendChild(a);
-        a.download = 'EarthQuickly v0.7.2.zip';
+        a.download = 'EarthQuickly v0.7.3.zip';
         a.href = url;
         a.click();
         a.remove();
